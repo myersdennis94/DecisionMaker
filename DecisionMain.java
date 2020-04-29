421a2a662c3746a8f3d8043045bd9d7f4438daed
Dennis Myers <djm174@txstate.edu>
2020-04-29 17:45:13 -0500

    Adds menu display

---
+++ DecisionMain.java
@@ -0,0 +1,15 @@
+package DecisionMaker;
+
+public class DecisionMain{
+
+	public printMenu(){
+		System.out.println("Decision Maker Program Options :");
+		System.out.println("\t1. Enter new option");
+		System.out.println("\t2. Erase existing option");
+		System.out.println("\t3. Run Decision Maker");
+	}
+
+	public static void main(String[] args){
+
+	}
+}
