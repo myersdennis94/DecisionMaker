4bc5fee7c82c32a02da7030adfc65f28bdb6270e
Dennis Myers <djm174@txstate.edu>
2020-04-30 17:08:41 -0500

    Adds menu functionality

--- DecisionMain.java
+++ DecisionMain.java
@@ -1,5 +1,8 @@
 package DecisionMaker;
 
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
 /**
  * Class to contain general functionality for DecisionMaker. Creation of
  * relevant objects, menu printing, UI handling.
@@ -11,11 +14,12 @@ public class DecisionMain{
 	 * Method to print menu options.
 	 * @author Dennis Myers
 	 */
-	public printMenu(){
+	public static void printMenu(){
 		System.out.println("Decision Maker Program Options :");
 		System.out.println("\t1. Enter new option");
 		System.out.println("\t2. Erase existing option");
 		System.out.println("\t3. Run Decision Maker");
+		System.out.println("\t4. Exit program");
 	}
 
 	/**
@@ -23,6 +27,40 @@ public class DecisionMain{
 	 * @author Dennis Myers
 	 */
 	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int response;
 
+		System.out.println("Welcome to Decision Maker!");
+		System.out.println("\nThe purpose of this program is to help you pick between different items.");
+		System.out.println("Enter some options to choose from and then run Decision Maker!\n");
+
+		while(true){
+			printMenu();
+			System.out.print(">");
+			try{
+				response = sc.nextInt();
+			}catch(InputMismatchException e){
+				System.out.println("\nInvalid input. Input must be an integer value.\n");
+				sc.next();
+				continue;
+			}
+			switch(response){
+				case 1:
+					System.out.println("\nIn enter option\n");
+					break;
+				case 2:
+					System.out.println("\nIn erase option\n");
+					break;
+				case 3:
+					System.out.println("\nIn run decision maker\n");
+					break;
+				case 4:
+					System.out.println("\nExiting program.");
+					System.out.println("Have a nice day!\n");
+					return;
+				default:
+					System.out.println("\nInvalid input. Input must match one of listed values.\n");
+			}
+		}
 	}
 }
