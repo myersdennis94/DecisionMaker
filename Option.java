4efe5720aeea4f1d2b2610299f829fae03406e37
Dennis Myers <djm174@txstate.edu>
2020-04-30 16:38:31 -0500

    Adds documentation

--- Option.java
+++ Option.java
@@ -1,17 +1,35 @@
 package DecisionMaker;
 
+/**
+ * Option class to hold option information and stats.
+ * @author Dennis Myers
+ */
 public class Option{
 	private String text;
 	private int numTimes = 0;
 
+	/**
+	 * Constructor for Option object.
+	 * @author Dennis Myers
+	 * @param text String representation of option.
+	 */
 	Option(String text){
 		this.text = text;
 	}
 
+	/**
+	 * Incrementer for numTimes.
+	 * @author Dennis Myers
+	 */
 	public void increment(){
 		numTimes++;
 	}
 
+	/**
+	 * Method to return class field text.
+	 * @author Dennis Myers
+	 * @return Class field text.
+	 */
 	public String getText(){
 		return text;
 	}
--- OptionHandler.java
+++ OptionHandler.java
@@ -1,22 +1,44 @@
 package DecisionMaker;
 
+/**
+ * Class to manage multiple options and their functionality.
+ * @author Dennis Myers
+ */
 public class OptionHandler{
 	private int numOptions;
 	private ArrayList <Option> options;
 
+	/**
+	 * Method to hold functionality for single decision.
+	 * @author 
+	 */
 	public void oneDecision(){
 
 	}
 
+	/**
+	 * Method to hold functionality for multiple runs before decision.
+	 * @author
+	 * @param runs Integer value of runs to be made before decision made.
+	 */
 	public void multiDecision(int runs){
 
 	}
 
+	/**
+	 * Method to add Option to options ArrayList.
+	 * @author
+	 * @param o Option object to be added to options ArrayList.
+	 */
 	public void addOption(Option o){
 		options.add(o);
 		numOptions++;
 	}
 
+	/**
+	 * Constructor for OptionHandler class.
+	 * @author Dennis Myers
+	 */
 	OptionHandler(){
 		numOptions = 0;
 		options = new ArrayList<Options>();
